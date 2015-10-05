e60307a335e2a08f94432304855f7e12eed4e227
seongminje <alswp1196@naver.com>
2015-10-05 18:35:42 +0900

    continuous character moves and monster1
    캐릭터 연속 움직임 및 몬스터1 움직이 구현

--- stage1 move.py
+++ stage1 move.py
@@ -6,23 +6,35 @@ def handle_events():
     global running
     global characterx
     global charactery
+    global keycheckup
+    global keycheckdown
+    global keycheckright
+    global keycheckleft
     events=get_events()
+
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_UP:
-                charactery=charactery+10
+                keycheckup=True
             elif event.key == SDLK_DOWN:
-                charactery=charactery-10
+                keycheckdown = True
             elif event.key == SDLK_LEFT:
-                characterx=characterx-10
+                keycheckleft = True
             elif event.key == SDLK_RIGHT:
-                characterx=characterx+10
-          #  elif event.type == SDL_MOUSEMOTION:
-          #      x,y=event.x, 600-event.y
-            elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+                keycheckright = True
+            elif event.key == SDLK_ESCAPE:
                 running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_UP:
+                keycheckup = False
+            elif event.key == SDLK_DOWN:
+                keycheckdown = False
+            elif event.key == SDLK_LEFT:
+                keycheckleft = False
+            elif event.key == SDLK_RIGHT:
+                keycheckright = False
     pass
 
 open_canvas(1600,900)
@@ -35,21 +47,35 @@ frame=0
 characterx=70
 charactery=160
 monster1x=1500
+keycheckup = False
+keycheckdown = False
+keycheckright = False
+keycheckleft = False
 
-while(characterx<800 and running):
+
+while(characterx<1500 and running):
     clear_canvas()
     for i in range(0,18):
         tile.clip_draw(14,176,90,60,45+i*90,30)
     character.clip_draw(frame*170,400,170,200,characterx,charactery)
     monster1.clip_draw(frame*70,254,70,46,monster1x,80)
-    update_canvas()
+
     monster1x-=10
     if monster1x<35:
         monster1x=1600
-
+    handle_events()
+    if keycheckup == True:
+        charactery=charactery+10
+    elif keycheckdown == True:
+        charactery=charactery-10
+    elif keycheckright == True:
+        characterx=characterx+10
+    elif keycheckleft == True:
+        characterx=characterx-10
+    update_canvas()
     frame=(frame+1)%6
     delay(0.05)
-    handle_events()
+
 
 close_canvas()
 
